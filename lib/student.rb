7d099bb024c4515e25a2569f60eeac9fc728ba0a
Thomas Pope <tpope0928@gmail.com>
2018-10-02 17:12:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,11 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |a, v|
+      self.send("#{a}=", v)
+    end
+    self
+  end
   end
 
   def self.all
