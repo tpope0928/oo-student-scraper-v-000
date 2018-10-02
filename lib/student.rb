53050d6e7500f7eee3665c5a9ced2b71a815ce90
Thomas Pope <tpope0928@gmail.com>
2018-10-02 17:05:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,8 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash.each do |
+    student_hash.each do |a, v|
+      
   end
 
   def self.create_from_collection(students_array)
