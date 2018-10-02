15a2b844c76d77499c7815344cf4664a271b02cd
Thomas Pope <tpope0928@gmail.com>
2018-10-02 17:07:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,8 +5,8 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash.each do |attribute, value|
-      self.send("#{attribute}=", value)
+    student_hash.each do |a, v|
+      self.send("#{a}=", v)
     end
     @@all << self
   end
